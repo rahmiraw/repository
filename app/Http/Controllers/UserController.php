9c627b1d8fe9aad06a2ba97dde738b0469f78a34
Rahmi Rahmawati <rahmawatirahmi2@gmail.com>
2023-11-27 18:27:10 +0700

    Submission 2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,13 +6,59 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     */
     public function index()
     {
-        return "ini adalah halaman user method index";
+        return "ini adalah isi dari methodnya";
     }
 
+    /**
+     * Show the form for creating a new resource.
+     */
     public function create()
     {
-        return "ini adalah halaman create method create";
+        return "ini adalah halaman user method create";
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($id)
+    {
+        return "ini adalah halaman tampilan user $id";
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit($id)
+    {
+        return "ini adalah halaman tampilan edit user $id";
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, $id)
+    {
+        return "ini adalah halaman update user dengan id: <b>".$id."</b>";
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($id)
+    {
+        return "ini adalah halaman delete user dengan id: <b>".$id."</b>";
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\UserController;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,8 +23,12 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-// Route::get('user', [UserController::class, 'index'])->name('user');
-// Route::get('/user/create', [UserController::class, 'create'])->name('user.create');
+Route::get('user', [UserController::class, 'index'])->name('user');
+Route::get('/user/create', [UserController::class, 'create'])->name('user.create');
+Route::get('/user/{id}', [UserController::class, 'show'])->name('user.show');
+Route::get('/user/{id}/edit', [UserController::class, 'edit'])->name('user.edit');
+Route::put('/user/{id}', [UserController::class, 'update'])->name('user.update');
+Route::get('/user/{id}/delete', [UserController::class, 'destroy'])->name('user.delete');
 
-Route::resource('user', UserController::class);
-Route::get('/user/caridata', [UserController::class, 'cariData'])->name('user.caridata');
+// Route::resource('user', UserController::class);
+// Route::get('/user/caridata', [UserController::class, 'cariData'])->name('user.caridata');
